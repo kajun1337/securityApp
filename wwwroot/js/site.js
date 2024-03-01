571ed7922d3c1d03d851b6190b07c2c9142b5959
Hakan Aydoğmuş <144350544+hakanaydogmus2@users.noreply.github.com>
2024-03-01 15:00:29 +0300

    refactored js files

---
+++ wwwroot/js/site.js
@@ -0,0 +1,39 @@
+
+
+async function sendLink() {
+
+    const link = document.getElementById("linkInput").value;
+    const uri = `http://localhost:5090/Link/SendLink?link=${link}`;
+    console.log(link);
+    const response = await fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            link: link
+        })
+    });
+    console.log(response);
+    if (response.ok) {
+        getResult(link);
+    } else {
+        console.error('Error sending link');
+    }
+}
+
+async function getResult(link) {
+    console.log("grdi");
+    const response = await fetch(`http://localhost:5090/Link/GetLinkResult?link=${encodeURIComponent(link)}`);
+    console.log(response);
+    if (response.ok) {
+        const data = await response.json();
+        document.getElementById("result").innerText = data;
+        console.log("take your jason");
+
+    } else if (response.status === 404) {
+        document.getElementById("result").innerText = "Link not found";
+    } else {
+        console.error('Error getting link result');
+    }
+}
